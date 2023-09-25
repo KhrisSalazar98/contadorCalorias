2a8fe771cb5f027733c6cc152b2b5b747f2f532b
cristobal <cristobal_adm_2015@outlook.com>
2023-09-25 15:09:15 -0300

    addTodayFood listo

--- src/hooks/useFoodStorage.ts
+++ src/hooks/useFoodStorage.ts
@@ -2,25 +2,22 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { Meal } from "../types";
 
 const MY_FOOD_KEY = '@MyFood:Key';
+const MY_TODAY_FOOD_KEY = '@MyTodayFood:Key';
 
 const useFoodStorage = () => {
 
-    const handleSaveFood = async ({calories, name, portion}: Meal) => {
-
+    const saveInfoToStorage = async (storageKey: string, meal: Meal) => {
+        
         try {
 
-            const currentSavedFood = await AsyncStorage.getItem(MY_FOOD_KEY);
+            const currentSavedFood = await AsyncStorage.getItem(storageKey);
         
             if(currentSavedFood !== null){
                 const currentSavedFoodPased = JSON.parse(currentSavedFood);
-                currentSavedFoodPased.push({
-                    calories,
-                    name,
-                    portion
-                });
+                currentSavedFoodPased.push(meal);
 
                 await AsyncStorage.setItem(
-                    MY_FOOD_KEY,
+                    storageKey,
                     JSON.stringify(currentSavedFoodPased)
                 );
 
@@ -28,14 +25,8 @@ const useFoodStorage = () => {
             }
 
             await AsyncStorage.setItem(
-                MY_FOOD_KEY,
-                JSON.stringify([
-                    {
-                        calories,
-                        name,
-                        portion,
-                    },
-                ]),
+                storageKey,
+                JSON.stringify([meal]),
             );
 
             return Promise.resolve();
@@ -43,9 +34,15 @@ const useFoodStorage = () => {
         } catch (error) {
             return Promise.reject();
         }
+    };
 
-        
-
+    const handleSaveFood = async ({calories, name, portion}: Meal) => {
+        try {
+            const result = await saveInfoToStorage(MY_FOOD_KEY, {calories, name, portion});
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error);
+        }
     };
 
     const handleGetFoods = async () => {
@@ -62,13 +59,32 @@ const useFoodStorage = () => {
         }
     };
 
+    const handleSaveTodayFood = async ({calories, name, portion}: Meal) => {
+        
+        try {
+            const result = await saveInfoToStorage(MY_TODAY_FOOD_KEY, {
+                calories,
+                name,
+                portion,
+                date: new Date().toISOString()
+            });
+            return Promise.resolve(result);
+        } catch (error) {
+            return Promise.reject(error)
+        }
+
+    }
+
     return {
         onSaveFood: handleSaveFood,
         onGetFoods: handleGetFoods,
+        onSaveTodayFood: handleSaveTodayFood
     };
 };
 
-// Guardar información de comida
-// Método para obtener info de comida
+// Guardar información de comida del día de hoy
+
+
+// Método para obtener info de comida del día de hoy
 
 export default useFoodStorage;
