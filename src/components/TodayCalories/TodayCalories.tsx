d1a017e24ab989f71642371cd869171f7326de8d
cristobal <cristobal_adm_2015@outlook.com>
2023-09-26 22:56:46 -0300

    todayCalories estilizado

--- src/components/TodayCalories/TodayCalories.tsx
+++ src/components/TodayCalories/TodayCalories.tsx
@@ -1,18 +1,49 @@
-import React from "react";
+import React, { FC } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import CircularProgress from 'react-native-circular-progress-indicator';
 
-const TodayCalories = () => {
+
+type TodayCaloriesProps = {
+    total: number | string;
+    consumed: number | string;
+    remaining: number | string;
+    percentage: number;
+}
+
+
+const TodayCalories: FC<TodayCaloriesProps> = ({
+    total = 0,
+    consumed = 0,
+    remaining = 0,
+    percentage = 0
+
+}) => {
 
     return (
         <View style={styles.container}>
-            <View>
+            <View style={styles.leftContainer}>
                 <CircularProgress 
-                    value={15}
+                    value={percentage}
                     valueSuffix={'%'}
                 />
             </View>
-            <View>
+            <View style={styles.rightContainer}>
+                <Text style={styles.today}>Today</Text>
+
+                <View style={styles.rightItem}>
+                    <Text style={styles.rightItemLegend}>Total</Text>
+                    <Text style={styles.rightItemValue}>{total}</Text>
+                </View>
+
+                <View style={styles.rightItem}>
+                    <Text style={styles.rightItemLegend}>Consumed</Text>
+                    <Text style={styles.rightItemValue}>{consumed}</Text>
+                </View>
+
+                <View style={styles.rightItem}>
+                    <Text style={styles.rightItemLegend}>Remaining</Text>
+                    <Text style={styles.rightItemValue}>{remaining}</Text>
+                </View>
 
             </View>
         </View>
@@ -21,7 +52,30 @@ const TodayCalories = () => {
 
 const styles = StyleSheet.create({
     container: {
-        
+        flexDirection: 'row'
+    },
+    leftContainer: {
+        flex: 1
+    },
+    rightContainer: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    today: {
+        fontSize: 20,
+        fontWeight: '500',
+        marginBottom: 14
+    },
+    rightItem: {
+        flexDirection: 'row',
+        marginBottom: 8
+    },
+    rightItemLegend: {
+        flex: 1
+    },
+    rightItemValue: {
+        flex: 1,
+        textAlign: 'right',
     }
 });
 
